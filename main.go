6adb9db52c15bfe09579e9eb3b62a8c0099d8e3a
wenjun <wenjun.wu@live.com>
2018-04-09 23:09:10 -0700

    fix log

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/wenwu449/vsts-pr/vsts"
 )
 
+const (
+	logPath = "LOG_PATH"
+)
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	logFilePath := os.Getenv(logPath)
+	if len(logFilePath) > 0 {
+
+		logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		mw := io.MultiWriter(os.Stdout, logFile)
+		log.SetOutput(mw)
+	}
 
 	config, err := vsts.GetConfig()
 	if err != nil {
@@ -22,7 +40,6 @@ func main() {
 	}
 
 	log.Printf("Got PR update: %v\n", pr.Resource.PullRequestID)
-	// image check
 
 	if !strings.EqualFold(pr.Resource.TargetRefName, fmt.Sprintf("%s/%s", "refs/heads", config.MasterBranch)) {
 		fmt.Printf("unexpected target branch: %s\n", pr.Resource.TargetRefName)
